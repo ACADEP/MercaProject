eaed921bf4a6d06a8cf3d7dc68add99cc55a6465
jonces <jonces94@hotmail.com>
2018-09-29 10:24:08 -0600

    Commit 29/09/18 v1

--- resources/views/cart/cart-payment.blade.php
+++ resources/views/cart/cart-payment.blade.php
@@ -139,7 +139,7 @@
                         // PayPal Client IDs - replace with your own
                         // Create a PayPal app: https://developer.paypal.com/developer/applications/create
                         client: {
-                        sandbox: 'AYPU5279dmgaIqbbk8_QrxzBk7Cv3HGVh4W_jQhqdugNDlVt8XP7gEC9tqSDRsvwfggevjmQqb-EU9zw',
+                        sandbox: 'AcbJmhLyQjcEbqe44-pfFrSk3UrV03SwoFSgFgwwoFfiCl8Qjda6ePlsHIyb0nCjzhOQDkUgsya5EHXn',
                         production: '<insert production client id>'
                         },
 
@@ -166,6 +166,15 @@
                         }
                         }, '#paypal-button-container');
                         </script>
+
+                        {{-- <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+                            <input type="hidden" name="cmd" value="_s-xclick">
+                            <input type="hidden" name="hosted_button_id" value="9VMXDFPYPU7EL">
+                            <input type="image" src="https://www.paypalobjects.com/es_XC/MX/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal, la forma más segura y rápida de pagar en línea.">
+                            <img alt="" border="0" src="https://www.paypalobjects.com/es_XC/i/scr/pixel.gif" width="1" height="1">
+                        </form> --}}
+                            
+
 
                     </div>
                 </div>
